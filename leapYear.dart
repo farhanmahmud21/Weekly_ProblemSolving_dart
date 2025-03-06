cdfc3326d7ed518052c145f97b36e97d87878728
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2025-03-06 16:59:06 +0600

    fix leapYear program logic to correctly identify leap years

--- leapYear.dart
+++ leapYear.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   String year = stdin.readLineSync()!;
   int inyear = int.parse(year);
-  if (inyear % 4 == 0) {
+  if (inyear % 400 == 0 || (inyear % 4 == 0 && inyear % 100 != 0)) {
     print('${inyear} is a leap year.');
   } else {
     print('${inyear} is not  leap year.');
