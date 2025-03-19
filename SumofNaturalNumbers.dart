23463bc7d0fe745449e8b76556c388491d7214fd
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2025-03-19 12:00:00 +0600

    Your commit message

--- SumofNaturalNumbers.dart
+++ SumofNaturalNumbers.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  int num = int.parse(stdin.readLineSync()!);
+  int? num = int.parse(stdin.readLineSync()!);
   int sum = 0;
   for (var i = 0; i <= num; i++) {
     sum += i;
---
+++ substitutionCiphar.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main(){
+  String input = stdin.readLineSync()!;
+  List inputStr=input!.split(" ");
+  String str=inputStr[0];
+  int num=inputStr[1];
+
+
+}
