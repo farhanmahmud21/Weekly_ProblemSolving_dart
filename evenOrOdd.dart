93fbf39fba11705feb92cf25075bb0f1092edaaa
Farhan Mahmud <mahmudkhanfarhan2001@gmail.com>
2025-03-24 23:07:54 +0600

    Update evenOrOdd.dart

--- evenOrOdd.dart
+++ evenOrOdd.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   int num = int.parse(stdin.readLineSync()!);
   if (num % 2 == 0) {
-    print('${num} is an even number.');
+    print('${num} is a even number.');
   } else {
     print('${num} is an odd number.');
   }
